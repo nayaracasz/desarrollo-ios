1324ee958f0364e8bd9b1d4d832dffbc62f91dda
nayaracasz <nayaracs208@gmail.com>
2024-11-13 10:58:32 -0600

    mod manzanas app

--- manzanas/manzanas/ViewController.swift
+++ manzanas/manzanas/ViewController.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchCaptura: UISwitch!
     @IBOutlet weak var txtManzanas: UITextField!
     @IBOutlet weak var lblTotalNaranjas: UILabel!
     @IBOutlet weak var lblManzanasRestantes: UILabel!
+    @IBOutlet weak var indicador: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         lblTotalNaranjas.text = "0"
         lblManzanasRestantes.text = "0"
+        indicador.startAnimating()
+        indicador.hidesWhenStopped = true
     }
 
     @IBAction func btnRealizarIntercambio(_ sender: Any) {
@@ -26,7 +30,49 @@ class ViewController: UIViewController {
         
         lblTotalNaranjas.text = "\(naranjas)"
         lblManzanasRestantes.text = "\(manzanasRestantes)"
+        if switchCaptura.isOn {
+            lblTotalNaranjas.textColor = .blue
+        } else {
+            lblTotalNaranjas.textColor = .brown
+        }
+        
+        if indicador.isAnimating{
+            print("indicador encendido :o")
+        } else {
+            print("indicador apagado :(")
+        }
+    }
+
+    @IBAction func swBloquearCaptura(_ sender: UISwitch) {
+        if switchCaptura.isOn {
+            print("Switch encendido :D")
+        } else {
+            print("Switch apagado :(")
+        }
+        
+        if sender.isOn {
+            print("Switch encendido :D")
+            txtManzanas.isEnabled = true
+            indicador.startAnimating()
+        } else {
+            print("Switch apagado :(")
+            txtManzanas.isEnabled = false
+            indicador.stopAnimating()
+        }
     }
     
+    @IBAction func SeleccionarSemestre(_ sender: UISlider) {
+        let semestre = Int(sender.value)
+        print(semestre)
+        print("Valor máximo: \(Int(sender.maximumValue))\nValor mínimo: \(Int(sender.minimumValue))")
+        
+        if semestre == 1 {
+            switchCaptura.setOn(false, animated: true)
+            txtManzanas.isEnabled = true
+        } else {
+            switchCaptura.setOn(true, animated: false)
+            txtManzanas.isEnabled = false
+        }
+    }
 }
 
