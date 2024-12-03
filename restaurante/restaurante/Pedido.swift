25b338c3b973adb0060fef7a6a6d1fba483e185b
nayaracasz <nayaracs208@gmail.com>
2024-12-03 09:23:50 -0600

    mod restaurante app

---
+++ restaurante/restaurante/Pedido.swift
@@ -0,0 +1,69 @@
+//
+//  Pedido.swift
+//  pedido
+//
+//  Created by nayara on 02/12/24.
+//
+
+class Pedido {
+    // atributos
+    private var hamburguesasSencillas: Double
+    private var hamburguesasConQueso: Double
+    private var papasFritas: Double
+    private var refrescos: Double
+    private var postres: Double
+    
+    // propiedades
+    var HamburguesasSencillas : Double {
+        get { return hamburguesasSencillas }
+        set { hamburguesasSencillas = newValue }
+    }
+    
+    var HamburguesasConQueso : Double {
+        get { return hamburguesasConQueso }
+        set { hamburguesasConQueso = newValue }
+    }
+    
+    var PapasFritas : Double {
+        get { return papasFritas }
+        set { papasFritas = newValue }
+    }
+    
+    var Refrescos : Double {
+        get { return refrescos }
+        set { refrescos = newValue }
+    }
+    
+    var Postres : Double {
+        get { return postres }
+        set { postres = newValue }
+    }
+    
+    //constructor
+    init() {
+        self.hamburguesasSencillas = 0
+        self.hamburguesasConQueso = 0
+        self.papasFritas = 0
+        self.refrescos = 0
+        self.postres = 0
+    }
+    
+//    init(_ hamburguesasSencillas : Double, _ hamburguesasConQueso : Double, _ papasFritas : Double, _ refrescos : Double, _ postres : Double) {
+//        self.hamburguesasSencillas = hamburguesasSencillas
+//        self.hamburguesasConQueso = hamburguesasConQueso
+//        self.papasFritas = papasFritas
+//        self.refrescos = refrescos
+//        self.postres = postres
+//    }
+    
+    //calcular total
+    func CalcularTotal() -> Double {
+        let totalHamburguesasSencillas = HamburguesasSencillas * 40
+        let totalHamburguesasConQueso = HamburguesasConQueso * 50
+        let totalPapasFritas = PapasFritas * 20
+        let totalResfrescos = Refrescos * 40
+        
+        let totalPostres = Postres * 40
+        return totalHamburguesasSencillas + totalHamburguesasConQueso + totalPapasFritas + totalResfrescos + totalPostres
+    }
+}
--- restaurante/restaurante/ViewController.swift
+++ restaurante/restaurante/ViewController.swift
@@ -7,13 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    // funciones para pickerview
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return menu.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return menu[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row {
+        case 0:
+            imgMenu.image = UIImage(named: "hamburguesa-sencilla")
+        case 1:
+            imgMenu.image = UIImage(named: "hamburguesa-queso")
+        case 2:
+            imgMenu.image = UIImage(named: "papas-fritas")
+        case 3:
+            imgMenu.image = UIImage(named: "soda")
+        case 4:
+            imgMenu.image = UIImage(named: "postre")
+        default:
+            imgMenu.image = nil
+        }
+    }
+    
+    // controles
+    @IBOutlet weak var imgMenu: UIImageView!
+    @IBOutlet weak var pkvMenu: UIPickerView!
+    @IBOutlet weak var stpCantidad: UIStepper!
+    @IBOutlet weak var swtCupon: UISwitch!
+    @IBOutlet weak var lblProductos: UILabel!
+    @IBOutlet weak var lblTotal: UILabel!
+    
+    //arreglo del menu de productos
+    let menu = ["Hamburguesa sencilla", "Hamburguesa con queso", "Papas fritas", "Refresco", "Postre"]
+    var mesaUno : Pedido
+    var mesaDos : Pedido
+    var mesaTres : Pedido
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pkvMenu.delegate = self
+        pkvMenu.dataSource = self
     }
-
-
+    
+    
+    
+    @IBAction func SeleccionarMesa(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            mesaUno = Pedido()
+        case 1:
+            mesaDos = Pedido()
+        case 2:
+            mesaTres = Pedido()
+        default:
+            break
+        }
+    }
+    
+    @IBAction func AgregarProducto(_ sender: Any) {
+        switch pkvMenu.selectedRow(inComponent: <#T##Int#>) {
+        case 0:
+            mesaUno.HamburguesasSencillas = (swtCupon.isOn) ? (
+        }
+    }
+    
+    @IBAction func AgregarPropina(_ sender: Any) {
+        
+    }
+    
+    @IBAction func CalcularTotal(_ sender: Any) {
+        
+    }
 }
-
