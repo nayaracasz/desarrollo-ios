17fdf5e6d05385fa0dd3609ed4345e955dbd94fd
nayaracasz <nayaracs208@gmail.com>
2024-12-03 10:54:00 -0600

    another mod restaurante app

--- restaurante/restaurante/ViewController.swift
+++ restaurante/restaurante/ViewController.swift
@@ -46,6 +46,8 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet weak var swtCupon: UISwitch!
     @IBOutlet weak var lblProductos: UILabel!
     @IBOutlet weak var lblTotal: UILabel!
+    @IBOutlet weak var lblCantidad: UILabel!
+    @IBOutlet weak var segMesas: UISegmentedControl!
     
     //arreglo del menu de productos
     let menu = ["Hamburguesa sencilla", "Hamburguesa con queso", "Papas fritas", "Refresco", "Postre"]
@@ -60,8 +62,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         pkvMenu.dataSource = self
     }
     
-    
-    
+    //seleccionar mesa
     @IBAction func SeleccionarMesa(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
@@ -75,10 +76,57 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         }
     }
     
+    // agregar producto
     @IBAction func AgregarProducto(_ sender: Any) {
-        switch pkvMenu.selectedRow(inComponent: <#T##Int#>) {
+        var cantidadProductos = Double(lblCantidad.text!) ?? 0.0
+        switch segMesas.selectedSegmentIndex {
         case 0:
-            mesaUno.HamburguesasSencillas = (swtCupon.isOn) ? (
+            switch pkvMenu.selectedRow(inComponent: <#T##Int#>) {
+            case 0:
+                mesaUno.HamburguesasSencillas = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 1:
+                mesaUno.HamburguesasConQueso = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 2:
+                mesaUno.PapasFritas = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 3:
+                mesaUno.Refrescos = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 4:
+                mesaUno.Postres = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            default:
+                break
+            }
+        case 1:
+            switch pkvMenu.selectedRow(inComponent: <#T##Int#>) {
+            case 0:
+                mesaDos.HamburguesasSencillas = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 1:
+                mesaDos.HamburguesasConQueso = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 2:
+                mesaDos.PapasFritas = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 3:
+                mesaDos.Refrescos = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 4:
+                mesaDos.Postres = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            default:
+                break
+            }
+        case 2:
+            switch pkvMenu.selectedRow(inComponent: <#T##Int#>) {
+            case 0:
+                mesaTres.HamburguesasSencillas = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 1:
+                mesaTres.HamburguesasConQueso = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 2:
+                mesaTres.PapasFritas = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 3:
+                mesaTres.Refrescos = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            case 4:
+                mesaTres.Postres = (swtCupon.isOn) ? (cantidadProductos / 2) : cantidadProductos
+            default:
+                break
+            }
+        default:
+            break
         }
     }
     
@@ -88,5 +136,10 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     @IBAction func CalcularTotal(_ sender: Any) {
         
+    }
+    
+    
+    @IBAction func ColocarCantidad(_ sender: UIStepper) {
+        lblCantidad.text = String(sender.value)
     }
 }
